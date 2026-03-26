2bdfcbb8351b7f665ba4c289bb87f6b5c1a5c150
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2026-03-26 11:14:13 -0700

    feat: added restrictive file reading

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(absolute_path, file_path))
+        valid_file_path = os.path.commonpath([absolute_path, target_file]) == absolute_path
+
+        if not valid_file_path:
+            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+            
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        f = open(target_file)
+        MAX_CHARS = 10000
+        file_content = f.read(MAX_CHARS)
+        
+        if f.read(1):
+            file_content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content
+            
+    except Exception as e:
+        return f'Error: {e}'
