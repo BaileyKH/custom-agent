542d79f31cb89ecc02e842c03602cc1944f289e6
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2026-03-26 08:56:32 -0700

    feat: added ability for agent to list contents of directory to view metadata

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(absolute_path, directory))
+        valid_target_path = os.path.commonpath([absolute_path, target_path]) == absolute_path
+
+        get_files = []
+
+        if not valid_target_path:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if not os.path.isdir(target_path):
+            return f'Error: "{directory}" is not a directory'
+        
+        for dir in os.listdir(target_path):
+            full_path = os.path.join(target_path, dir)
+            file_size = os.path.getsize(full_path)
+            is_dir = os.path.isdir(full_path)
+
+            get_files.append(f'- {dir}: file_size={file_size} bytes, is_dir={is_dir}')
+
+        return '\n'.join(get_files)
+    except Exception as e:
+        return f'Error: {e}'
